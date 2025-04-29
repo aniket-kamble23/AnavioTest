bb74ec032a4d070bee823d625d2620696158cb9e
Luis A Reyes <luisareyes_work@Luiss-MBP-16.lan>
2025-04-29 14:31:09 -0700

    Changed base url to dev environment instead of local, update gitignore file to ignore DS_Store and Cypress Downloads folder

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200/",
-    // baseUrl: "https://ui.anavio.ai",
+    // baseUrl: "http://localhost:4200/",
+    baseUrl: "https://ui.anavio.ai",
     viewportWidth: 1440,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
