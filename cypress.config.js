7158673d8c214e67b09890accffac989a8982a3a
aniket-kamble23 <144991083+aniket-kamble23@users.noreply.github.com>
2025-05-06 17:00:14 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+	viewportWidth: 1280,
+    	viewportHeight: 800,
+    	setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern:'cypress/integration/examples/*.js'
