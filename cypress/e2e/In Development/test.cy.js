0d12ac228664df525d3bcdddd58644bce0438130
lreyes-qae <lreyes@vicon-security.com>
2025-05-07 17:07:54 -0700

    Added simple test cases to the Directory, Groups, Holidays and Sites pages.

--- cypress/e2e/In Development/test.cy.js
+++ cypress/e2e/In Development/test.cy.js
@@ -1,4 +1,4 @@
-describe("Sites Page Tests", () => {
+describe("Directory Page Tests", () => {
   before(() => {
     Cypress.session.clearAllSavedSessions();
     cy.clearCookies();
@@ -14,28 +14,42 @@ describe("Sites Page Tests", () => {
         cy.login(credentials.validUser.email, credentials.validUser.password);
       });
     });
-    cy.log("Session restored, navigating to Sites...");
-    cy.visit("/settings/sites");
+    cy.log("Session restored, navigating to Directory...");
+    cy.visit("/settings/directory");
   });
 
-  it("should do something :)", () => {
-    cy.get(".anavio-header")
-      .should("exist");
-  });  
-
-  it("should log out when the Log out option is clicked", () => {
-    cy.logout();
-    cy.url().should("include", "/auth/sign-in");
+  it("should display all required Directory container elements", () => {
+    cy.get(".directory-list-container").within(() => {
+      cy.get(".directory-header-wrapper").within(() => {
+        cy.get("h2").should("be.visible").contains("Directory");
+        cy.get(".search-bar")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".search-input")
+              .should("be.visible")
+              .should("have.attr", "placeholder", "Search...");
+            cy.get(".mat-icon")
+              .should("be.visible")
+              .find("svg")
+              .should("be.visible");
+          });
+      });
+    });
   });
+
+  // it("should log out when the Log out option is clicked", () => {
+  //   cy.logout();
+  //   cy.url().should("include", "/auth/sign-in");
+  // });
 });
 
-// Custom Command for Logout
-Cypress.Commands.add("logout", () => {
-  cy.get(
-    ".anavio-user-account-icon > .mat-mdc-menu-trigger > .mat-icon"
-  ).click();
-  cy.get(".mat-mdc-menu-content > :nth-child(7)").click();
-  cy.get(".mat-mdc-dialog-actions").within(() => {
-    cy.contains("Logout").click();
-  });
-});
+// // Custom Command for Logout
+// Cypress.Commands.add("logout", () => {
+//   cy.get(
+//     ".anavio-user-account-icon > .mat-mdc-menu-trigger > .mat-icon"
+//   ).click();
+//   cy.get(".mat-mdc-menu-content > :nth-child(7)").click();
+//   cy.get(".mat-mdc-dialog-actions").within(() => {
+//     cy.contains("Logout").click();
+//   });
+// });
--- cypress/e2e/Sanity Suite/directory.cy.js
+++ cypress/e2e/Sanity Suite/directory.cy.js
@@ -1,41 +1,93 @@
 describe("Directory Page Tests", () => {
-    before(() => {
-      Cypress.session.clearAllSavedSessions();
-      cy.clearCookies();
-      cy.clearLocalStorage();
-      cy.window().then((win) => {
-        win.sessionStorage.clear();
+  before(() => {
+    Cypress.session.clearAllSavedSessions();
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.window().then((win) => {
+      win.sessionStorage.clear();
+    });
+  });
+
+  beforeEach(() => {
+    cy.session("user-session", () => {
+      cy.fixture("credentials").then((credentials) => {
+        cy.login(credentials.validUser.email, credentials.validUser.password);
       });
     });
-  
-    beforeEach(() => {
-      cy.session("user-session", () => {
-        cy.fixture("credentials").then((credentials) => {
-          cy.login(credentials.validUser.email, credentials.validUser.password);
+    cy.log("Session restored, navigating to Directory...");
+    cy.visit("/settings/directory");
+  });
+
+  it("should display all required Header UI elements", () => {
+    // Verify Header
+    cy.get(".anavio-header").within(() => {
+      cy.get(".back-button")
+        .should("be.visible")
+        .find("mat-icon > svg")
+        .should("be.visible");
+      cy.get(".anavio-header-title").should("be.visible").contains("Directory");
+      cy.get(".breadcrumb-home-icon")
+        .should("be.visible")
+        .find("mat-icon > svg")
+        .should("be.visible");
+      cy.get("app-breadcrumbs > .icon").should("be.visible");
+      cy.get(".breadcrumb > a").should("be.visible").contains("Directory");
+      cy.get(".anavio-header-actions")
+        .should("be.visible")
+        .find(".mat-mdc-menu-trigger > .mat-icon > svg")
+        .should("be.visible");
+    });
+  });
+
+  it("should display all required Directory Card container elements", () => {
+    cy.get(".add-directory-card").within(() => {
+      cy.get(".directory-banner > span")
+        .should("be.visible")
+        .contains("Create a directory where people can contact users.");
+
+      cy.get(".add-directory-button")
+        .should("be.visible")
+        .within(() => {
+          cy.get("app-anavio-icon > .anavio-icon > .mat-icon > svg").should(
+            "be.visible"
+          );
+          cy.contains("Add Entry");
         });
+    });
+  });
+
+  it("should display all required Directory container elements", () => {
+    cy.get(".directory-list-container").within(() => {
+      cy.get(".directory-header-wrapper").within(() => {
+        cy.get("h2").should("be.visible").contains("Directory");
+        cy.get(".search-bar")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".search-input")
+              .should("be.visible")
+              .should("have.attr", "placeholder", "Search...");
+            cy.get(".mat-icon")
+              .should("be.visible")
+              .find("svg")
+              .should("be.visible");
+          });
       });
-      cy.log("Session restored, navigating to Directory...");
-      cy.visit("/settings/directory");
-    });
-  
-    it("should do something :)", () => {
-      cy.get(".anavio-header")
-        .should("exist");
-    });  
-  
-    it("should log out when the Log out option is clicked", () => {
-      cy.logout();
-      cy.url().should("include", "/auth/sign-in");
     });
   });
-  
-  // Custom Command for Logout
-  Cypress.Commands.add("logout", () => {
-    cy.get(
-      ".anavio-user-account-icon > .mat-mdc-menu-trigger > .mat-icon"
-    ).click();
-    cy.get(".mat-mdc-menu-content > :nth-child(7)").click();
-    cy.get(".mat-mdc-dialog-actions").within(() => {
-      cy.contains("Logout").click();
-    });
+
+  it("should log out when the Log out option is clicked", () => {
+    cy.logout();
+    cy.url().should("include", "/auth/sign-in");
   });
+});
+
+// Custom Command for Logout
+Cypress.Commands.add("logout", () => {
+  cy.get(
+    ".anavio-user-account-icon > .mat-mdc-menu-trigger > .mat-icon"
+  ).click();
+  cy.get(".mat-mdc-menu-content > :nth-child(7)").click();
+  cy.get(".mat-mdc-dialog-actions").within(() => {
+    cy.contains("Logout").click();
+  });
+});
