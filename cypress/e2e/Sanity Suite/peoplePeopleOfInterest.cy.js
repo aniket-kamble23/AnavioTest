489230ce56fdeeddc5886e14f0727e8d08f828ac
lreyes-qae <lreyes@vicon-security.com>
2025-06-16 11:23:42 -0700

    Completed the People – People of Interest page tests

--- cypress/e2e/Sanity Suite/peoplePeopleOfInterest.cy.js
+++ cypress/e2e/Sanity Suite/peoplePeopleOfInterest.cy.js
@@ -111,6 +111,144 @@ describe("People > People of Interest Page Tests", () => {
     });
   });
 
+  it("should display the filter container elements", () => {
+    cy.get(".filter-wrapper").within(() => {
+      cy.get("app-searchbar")
+        .should("be.visible")
+        .within(() => {
+          cy.get("mat-icon").should("exist").find("svg").should("be.visible");
+          cy.get("input.search-input")
+            .should("be.visible")
+            .and("have.attr", "placeholder", "Search...");
+        });
+      cy.get("app-sort-menu").within(() => {
+        cy.get("button.filter-button")
+          .should("be.visible")
+          .within(() => {
+            cy.get("mat-icon").should("exist").find("svg").should("be.visible");
+            cy.get(".mdc-button__label").should("contain.text", "Sort");
+          });
+      });
+    });
+  });
+
+  it("should display the People of Interest grid", () => {
+    cy.get(".people-list").should("be.visible");
+  });
+
+  it("should display between 1 and 25 People of Interest card elements", () => {
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card").then((cards) => {
+        const count = cards.length;
+        expect(count).to.be.gte(1);
+        expect(count).to.be.lte(25);
+      });
+    });
+  });
+
+  it("should display a person icon and 3-dot menu button with the correct icon in each People of Interest card header", () => {
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card").each(($card) => {
+        cy.wrap($card)
+          .find("div.header")
+          .within(() => {
+            cy.get('mat-icon[data-mat-icon-name="person-of-interest"]')
+              .should("exist")
+              .within(() => {
+                cy.get("svg").should("exist");
+              });
+            cy.get("button.menu-button")
+              .should("exist")
+              .within(() => {
+                cy.get("mat-icon")
+                  .should("have.attr", "data-mat-icon-name", "more-vertical")
+                  .within(() => {
+                    cy.get("svg").should("exist");
+                  });
+              });
+          });
+      });
+    });
+  });
+
+  it("should display an option with an icon in the first People of Interest card 3-dot menu", () => {
+    // Open the menu of the first card
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card")
+        .first()
+        .within(() => {
+          cy.get("button.menu-button").click();
+        });
+    });
+
+    // Wait for the panel to appear in the DOM
+    cy.get(".mat-mdc-menu-panel")
+      .should("be.visible")
+      .within(() => {
+        const menuItems = [{ name: "Remove Person of Interest", index: 0 }];
+
+        // Loop through the menu items for both checks
+        menuItems.forEach(({ name, index }) => {
+          cy.get("button.mat-mdc-menu-item")
+            .eq(index)
+            .should("be.visible")
+            .should("contain.text", name)
+            .within(() => {
+              cy.get("mat-icon").find("svg").should("be.visible");
+            });
+        });
+      });
+  });
+
+  it("should display either an image or a fallback icon in each People of Interest card", () => {
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card").each(($card) => {
+        cy.wrap($card).within(() => {
+          cy.get("div.content > button").then(($button) => {
+            // Look for the image first
+            const $img = $button.find('img[alt="profile picture"]');
+            if ($img.length > 0) {
+              cy.wrap($img).should("exist");
+            } else {
+              // If no image, look for the fallback icon
+              cy.wrap($button)
+                .find("div.no-photo-fallback")
+                .should("exist")
+                .within(() => {
+                  cy.get("div.user-initials").should("exist");
+                });
+            }
+          });
+        });
+      });
+    });
+  });
+
+  it("should display a person's name in each People of Interest card", () => {
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card").each(($card) => {
+        cy.wrap($card)
+          .find("div.person-name")
+          .should("exist")
+          .invoke("text")
+          .then((text) => {
+            expect(text.trim().length).to.be.greaterThan(0);
+          });
+      });
+    });
+  });
+
+  it("should display View Activity button in each People of Interest card footer", () => {
+    cy.get(".people-list").within(() => {
+      cy.get("app-people-person-card").each(($card) => {
+        cy.wrap($card)
+          .find("button.view-activity-button")
+          .should("exist")
+          .and("contain.text", "View Activity");
+      });
+    });
+  });
+
   it("should log out when the Log out option is clicked", () => {
     cy.logout();
     cy.url().should("include", "/auth/sign-in");
